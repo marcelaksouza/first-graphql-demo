f6e62fa0983c229ff33efac60dae32db71d8c3c8
marcelaksouza <ma.karoline@hotmail.com>
2021-05-26 11:06:25 +0100

    db connection"

--- server/app.js
+++ server/app.js
@@ -1,14 +1,24 @@
+require('dotenv').config()
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4001;
+const URL = process.env.DB_SERVER+"://"+process.env.DB_USER+":"+process.env.DB_PASS+"@cluster0.kespz.mongodb.net/"+process.env.DB_NAME+"?retryWrites=true&w=majority"
 
-// mongoose.connect(process.env.URL);
-// mongoose.connection.once('open',()=>{
-//     console.log('connected to database')
-// })
+const options ={
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}
+
+mongoose.connect(URL, options).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */ },
+    err => { consolw.log(err) }
+  );
+mongoose.connection.once('open',()=>{
+    console.log('connected to database')
+})
 
 app.use('/graphql',graphqlHTTP({
     schema,
